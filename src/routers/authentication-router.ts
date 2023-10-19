61e59656ac3764ba80854690210f65ac23c545c1
Schwartz <darlanswtz@gmail.com>
2023-10-18 23:29:40 -0300

    Feat: githublogin routes

--- src/routers/authentication-router.ts
+++ src/routers/authentication-router.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-import { singInPost } from '@/controllers';
+import { getAccessToken, singInPost } from '@/controllers';
 import { validateBody } from '@/middlewares';
 import { signInSchema } from '@/schemas';
 
 const authenticationRouter = Router();
 
 authenticationRouter.post('/sign-in', validateBody(signInSchema), singInPost);
-
+authenticationRouter.get('/acess-token', getAccessToken);
 export { authenticationRouter };
